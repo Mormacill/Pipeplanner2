3fc35991a23e641f62e06a81a16933b1aab0d6dc
Mormacill <bachfreak15@googlemail.com>
2019-03-11 10:00:00 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+//Variablendeklarierung
+
+int n_seg = 0;
+
+cout << "Druckverlustrechner für Rohrstrecken mit dem Medium Luft  v1.0" << endl << endl;
+cout << "Bitte geben Sie an, aus wie vielen Segmenten Ihre Rohrstrecke besteht" << endl << endl;
+cin >> n_seg;
+cout << endl;
+
+double Druckarray[n_seg];
+/*int i;
+
+for (i=0; i<n_seg; i++)
+{
+  Druckarray[i] = 2;
+}
+
+for (i=0; i<n_seg; i++)
+{
+cout << Druckarray[i] << endl;
+}
+cout << "Bitte wählen sie Ihr erstes Streckensegment aus!" << endl;
+*/
+
+
+
+}
