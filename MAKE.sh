5eaa6cf6a4f314dc95fd585bf545e3eb567e5c73
Mormacill <bachfreak15@googlemail.com>
2019-03-14 23:22:28 +0100

    first commit

--- MAKE.sh
+++ MAKE.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-sudo apt install gnuplot build-essential
+if ! gnuplot > /dev/null; then
+   echo -e "Gnuplot wurde noch nicht auf Ihrem System installiert. Möchten Sie dies jetzt tun? (y/n) \c"
+   read
+   if "$REPLY" = "y"; then
+      sudo apt install gnuplot -y
+   fi
+fi
+
+if ! c++ > /dev/null; then
+   echo -e "C++ wurde noch nicht auf Ihrem System installiert. Möchten Sie dies jetzt tun? (y/n) \c"
+   read
+   if "$REPLY" = "y"; then
+      sudo apt install build-essential -y
+   fi
+fi
+
+echo -e "Pipeplanner wird nun gebaut."
+
 make
 sleep 1
 make clean
