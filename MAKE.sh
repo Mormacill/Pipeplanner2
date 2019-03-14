349c3432f4ceae2229ce72b12b3d65d7ad74ea25
Mormacill <bachfreak15@googlemail.com>
2019-03-14 23:41:12 +0100

    first commit

--- MAKE.sh
+++ MAKE.sh
@@ -6,7 +6,7 @@ if [ $? -eq 0 ]; then
     echo "Gnuplot ist installiert!"
 else
     echo "Gnuplot ist noch nicht installiert, möchte Sie dies jetzt tun? (y/n)"
-    read -p antwort
+    read -p "" antwort
     if antwort = "y"; then
     sudo apt install gnuplot
     fi
@@ -18,7 +18,7 @@ if [ $? -eq 0 ]; then
     echo "C++ ist installiert!"
 else
     echo "C++ ist noch nicht installiert, möchte Sie dies jetzt tun? (y/n)"
-    read -p antwort
+    read -p "" antwort
     if antwort = "y"; then
     sudo apt install build-essential
     fi
