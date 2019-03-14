1367453de75b454bb60eff406a2d5cba4d92a564
Mormacill <bachfreak15@googlemail.com>
2019-03-14 23:56:08 +0100

    first commit

--- MAKE.sh
+++ MAKE.sh
@@ -3,24 +3,32 @@
 dpkg -s gnuplot &> /dev/null
 
 if [ $? -eq 0 ]; then
+    echo "
+    "
     echo "Gnuplot ist installiert!"
 else
+    echo "
+    "
     echo "Gnuplot ist noch nicht installiert, möchte Sie dies jetzt tun? (y/n) "
     read ANTWORT
     if [ "$ANTWORT" == "y" ]; then
-    sudo apt install gnuplot
+    sudo apt install gnuplot -y
     fi
 fi
 
 dpkg -s build-essential &> /dev/null
 
 if [ $? -eq 0 ]; then
+    echo "
+    "
     echo "C++ ist installiert!"
 else
+    echo "
+    "
     echo "C++ ist noch nicht installiert, möchte Sie dies jetzt tun? (y/n) "
     read ANTWORT
     if [ "$ANTWORT" == "y" ]; then
-    sudo apt install build-essential
+    sudo apt install build-essential -y
     fi
 fi
 
